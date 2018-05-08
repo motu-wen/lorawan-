b325916ede1645c07469847a42978ff57b53bc08
Martin-wwh <1454565178@qq.com>
2018-05-08 16:45:20 +0800

    third push

---
+++ src/main/java/com/example/lorawan/doamin/ClassRoom.java
@@ -0,0 +1,16 @@
+package com.example.lorawan.doamin;
+
+import java.util.List;
+
+/**
+ * 教室的实体类
+ *
+ * @author {Administrator}
+ * @data 2018/5/8 16:17
+ **/
+public class ClassRoom {
+    private Integer build;//教学楼编号
+    private Integer floor;
+    private Integer room;
+    private List<Integer> doors;
+}
--- src/main/java/com/example/lorawan/enmu/redisExplain.java
+++ src/main/java/com/example/lorawan/enmu/redisExplain.java
@@ -7,7 +7,6 @@ public enum redisExplain {
 	type("key为type跟devaddr值，如type8888，type417544，value记录该id设备的类型","type"),
 	loraWanIp("loraWan的IP与Port,用来记录是否已连接","loraWanIp"),
 	status("key为status跟devaddr值，如status8888，status417544，value记录该id设备的解析数据","status"),
-
 	hongwaiList("所有加入过缓存的红外传感器","hongwaiList"),
 	zhinengsuoList("所有加入过缓存的智能锁传感器","zhinengsuoList"),
 	humitureList("所有加入过缓存的温湿度传感器","humitureList"),
@@ -15,12 +14,6 @@ public enum redisExplain {
 	parkingList("所有加入过缓存的地磁传感器","parkingList"),
 	manholecoverList("所有加入过缓存的窨井盖传感器","manholecoverList"),
 	assetsList("所有加入过缓存的资产定位传感器","assetsList"),
-	
-	thresholdWDMax("温度最大值","thresholdWDMax"),
-	thresholdWDMin("温度最小值","thresholdWDMin"),
-	thresholdSDMax("湿度最大值","thresholdSDMax"),
-	thresholdSDMin("湿度最小值","thresholdSDMin"),
-	
 	lorawanIpAndPort("Lora服务端Ip:Port","lorawanIpAndPort"),
 	lorawanAddress("Lora服务端Address","lorawanAddress"),
 	lorawanRestNum("Lora服务端重连次数","lorawanRestNum");
