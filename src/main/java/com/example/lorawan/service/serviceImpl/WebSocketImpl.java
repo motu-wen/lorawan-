56a6ba2c8d41debc0e11251d0487ee4cc8a25d5c
martin-wwh <1454565178@qq.com>
2018-05-09 15:38:13 +0800

    wwh push

--- src/main/java/com/example/lorawan/service/serviceImpl/WebSocketImpl.java
+++ src/main/java/com/example/lorawan/service/serviceImpl/WebSocketImpl.java
@@ -3,7 +3,6 @@ package com.example.lorawan.service.serviceImpl;
 import com.example.lorawan.doamin.InfraredSentor;
 import com.example.lorawan.enmu.redisExplain;
 import com.example.lorawan.service.WebSocketServer;
-
 import com.example.lorawan.until.Hex;
 import com.example.lorawan.until.JSonUtil;
 import org.slf4j.Logger;
@@ -14,7 +13,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -41,13 +39,11 @@ public class WebSocketImpl implements WebSocketServer {
 
         String IRCount=data.substring(16,data.length()-1);
         String   peopleCount=Hex.HexToString(IRCount);
-        System.out.println(peopleCount);
         InfraredSentor infraredSentor=new InfraredSentor();
         String type = stringRedisTemplate.opsForValue().get(redisExplain.type.explain + devaddr);
         infraredSentor.setDevaddr(devaddr);
         infraredSentor.setCount(peopleCount);
         infraredSentor.setTime(new SimpleDateFormat("MM-dd HH:mm:ss").format(new Date()));
-        infraredSentor.setType("222");
         String hongwaiList=stringRedisTemplate.opsForValue().get(redisExplain.hongwaiList.explain);
         if(StringUtils.isEmpty(hongwaiList)){
             hongwaiList=devaddr;
