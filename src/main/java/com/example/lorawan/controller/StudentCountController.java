9c2ada74638c457ffb34ef5f154f76c9fc284ecf
123456 <123456@qq.com>
2018-05-24 18:10:50 +0800

    Merge branch 'master' of https://github.com/Martin-wwh/lorawan
    # Conflicts:
    #	src/test/java/com/example/lorawan/TestRedis.java

--- src/main/java/com/example/lorawan/controller/StudentCountController.java
+++ src/main/java/com/example/lorawan/controller/StudentCountController.java
@@ -23,35 +23,35 @@ public class StudentCountController {
     private StringRedisTemplate stringRedisTemplate;
     @Autowired
     private RoomServerImpl roomServer;
-    @RequestMapping(value = "/getbuildall",method = RequestMethod.POST)
+    @RequestMapping(value = "/getbuildall",method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity getBuildAll(){
-        List<String> buildall=stringRedisTemplate.opsForList().range("build",0,-1);
+        List<String> buildall=stringRedisTemplate.opsForList().range("buildall",0,-1);
         return ResponseEntity.success().add("list",buildall);
     }
-    @RequestMapping(value = "/getfloorall",method = RequestMethod.POST)
+    @RequestMapping(value = "/getfloorall",method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity getFloorAll(@RequestBody String build){
+    public ResponseEntity getFloorAll(@RequestParam String build){
         List<String> floorall=stringRedisTemplate.opsForList().range(build+"build",0,-1);
         return ResponseEntity.success().add("list",floorall);
     }
-    @RequestMapping(value = "/getroomdall",method = RequestMethod.POST)
+    @RequestMapping(value = "/getroomdall",method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity getRoomAll(@RequestBody String floor){
+    public ResponseEntity getRoomAll(@RequestParam String floor){
         List<String> roomall=stringRedisTemplate.opsForList().range(floor+"floor",0,-1);
         return ResponseEntity.success().add("list",roomall);
     }
     @RequestMapping(value = "/getStudentCount",method = RequestMethod.POST)
     @ResponseBody
-    public ClassRoom getStudentCount(@RequestBody ClassRoom classRoom){
-        String key=classRoom.getBuild()+"-"+classRoom.getFloor()+"-"+classRoom;
+    public ResponseEntity getStudentCount(@RequestParam String key){
         ClassRoom classRoom1=JSonUtil.toObject(stringRedisTemplate.opsForValue().get(key),ClassRoom.class);
-        return classRoom1;
+        Integer count=classRoom1.getCount();
+        return ResponseEntity.success().add("list",count);
     }
 
     @RequestMapping(value = "/addRoom",method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity saveClassRoom( ClassRoom classRoom){
+    public ResponseEntity saveClassRoom(ClassRoom classRoom){
         roomServer.saveClassRoom(classRoom);
         return ResponseEntity.success();
     }
